45abd7a2a12a4a988eba15112c749f5601450b0c
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-28 15:07:08 -0400

    Menu

--- 01-web-tailwind/tailwind.config.js
+++ 01-web-tailwind/tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
       backgroundImage: {
         'body-pattern': "url('../img/pattern.png')",
       },
+      colors: {
+        'azul-claro': '#37bcf9',
+        'azul-oscuro': '#0370b9',
+      },
     },
   },
   plugins: [],
